eadd97beabf119b21ed00fa3f9a0ffe26ee3bca2
Vanisha1606 <vanishag1606@gmail.com>
2026-06-28 18:33:51 +0530

    Update Bundle ID to com.aspiring.BalanceSheet and version to 9.0

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.ionicframework.balancesheet139047',
+  appId: 'com.aspiring.BalanceSheet',
   appName: 'BalanceSheet',
   webDir: 'dist',
   ios: {
